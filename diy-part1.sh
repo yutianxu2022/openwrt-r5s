cc1e5760feca9da9d7f8a852ca7e1b8356b39cec
yutianxu2022 <116957550+yutianxu2022@users.noreply.github.com>
2022-11-27 14:12:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,7 +5,7 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
+https://github.com/coolsnowwolf/lede
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
